b2e74bb85994eae1f0fa60e713bcf8f0f5cf9d2b
Oleksandr Deriabin <aderiabin@luxoft.com>
2019-05-21 16:52:30 +0300

    Update of common module for multiple modules feature

--- test_scripts/RC/MultipleModules/commonRCMulModules.lua
+++ test_scripts/RC/MultipleModules/commonRCMulModules.lua
@@ -1,3 +1,28 @@
 local commonRC = require('test_scripts/RC/commonRC')
+local actions = require("user_modules/sequences/actions")
+local hmi_values = require("user_modules/hmi_values")
 
-return commonRC
+local common = {}
+
+common.registerAppWOPTU = actions.app.registerNoPTU
+common.activateApp = actions.app.activate
+
+function common.getDefaultRcCapabilities()
+	local hmiCapabilities = hmi_values.getDefaultHMITable()
+	local hmiRcCapabilities = hmiCapabilities.RC.GetCapabilities.params.remoteControlCapability
+	local rcCapabilities = {}
+	for moduleType, capabilitiesParamName in pairs(commonRC.capMap) do
+		rcCapabilities[moduleType] = hmiRcCapabilities[capabilitiesParamName]
+	end
+	return rcCapabilities
+end
+
+function common.start(pRcCapabilities)
+	local hmiCapabilities
+	if pRcCapabilities then
+		hmiCapabilities = commonRC.buildHmiRcCapabilities(pRcCapabilities)
+	end
+	return actions.start(hmiCapabilities)
+end
+
+return common
